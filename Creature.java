36b196f3599a0a24bc786cc27dd66c75c788ddde
andrew-helgeson <43154691+andrew-helgeson@users.noreply.github.com>
2018-11-16 12:06:18 -0500

    Edited Creature class, implemented random damage, added isAlive()

--- Creature.java
+++ Creature.java
@@ -1,22 +1,41 @@
-
+import java.util.Random;
 /**
  * Write a description of class Creature here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Andrew Helgeson
+ * @version 11.16.2018
  */
 public class Creature
 {
-   private int hp;
-   private int strength;
+   private int hp = 10;
+   private int strength = 10;
+   private Random rand;
    
-   public Creature(){
+   //constructor takes strength and hitpoints
+   public Creature(int hitpoints, int str){
+       hp = hitpoints;
+       strength = str;
+       rand = new Random();
        
    }
-    
+   
+   //damage() method deals a random damage number between 1 and strength
    public int damage(){
-       //TODO: change this
-       return 0;
+       int rand_damage = rand.nextInt((strength - 1)) + 1;
+       return rand_damage;
     }
+   
+    
+    
+   //isAlive() returns true if hp > 0
+   public boolean isAlive()
+   {
+      if (hp > 0)
+      {
+          return true;
+      }
+      else
+          return false;
+   }
     
 }
